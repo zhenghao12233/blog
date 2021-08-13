968a75d75f4c8dd2428686ce809a157be17ca2d0
zhenghao12233 <1223363269@qq.com>
2021-08-13 17:40:07 +0800

    第二天

--- src/components/LeftMain.tsx
+++ src/components/LeftMain.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 const LeftMain = () => {
     return (
         <div className="left_main">
-            <div className="swiper-box">
-                <div className="swiper"></div>
-                <div></div>
+            <div className="swiper_box">
+                <div className="swiper_img"></div>
+                <div className="swiper_article"></div>
             </div>
         </div>
     );
