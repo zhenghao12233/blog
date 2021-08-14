8e9b0d17b3bc2ea9fc3b1c958392b58e2237309e
郑好 <zhenghao@192.168.0.4>
2021-08-14 22:21:52 +0800

    第三天

--- src/components/LeftMain/LeftMain.tsx
+++ src/components/LeftMain/LeftMain.tsx
@@ -1,33 +1,268 @@
-import React from 'react';
+import React, { useEffect, useState, Component } from 'react';
 import './index.css'
+import {
+    LeftOutlined,
+    RightOutlined
+} from '@ant-design/icons';
 
-const LeftMain = () => {
-    return (
-        <div className="left_main">
-            <div className="swiper_box">
-                <ul className="swiper_img">
-                    <li>
-                        <img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg11.51tietu.net%2Fpic%2F2016-071418%2F20160714181543xyu10ukncwf221991.jpg&refer=http%3A%2F%2Fimg11.51tietu.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1631447252&t=78c6dadf4b5ad3c0ca3577c2ba28e48f" alt="" />
-                    </li>
-                    <li>
-                        <img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fwx3.sinaimg.cn%2Flarge%2F547f46d9ly4gsqmhfbuf1j20u00g2whd.jpg&refer=http%3A%2F%2Fwx3.sinaimg.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1631447252&t=3383361b9364fe26b14a793387391a46" alt="" />
-                    </li>
-                    <li>
-                        <img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fgss0.baidu.com%2F-Po3dSag_xI4khGko9WTAnF6hhy%2Fzhidao%2Fpic%2Fitem%2F4034970a304e251fae75ad03a786c9177e3e534e.jpg&refer=http%3A%2F%2Fgss0.baidu.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1631447252&t=8129cb5afafaaeaa4a22ca455283a7dd" alt="" />
-                    </li>
-                    <li>
-                        <img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.pconline.com.cn%2Fimages%2Fupload%2Fupc%2Ftx%2Fphotoblog%2F1309%2F25%2Fc49%2F26316176_26316176_1380092693834_mthumb.jpg&refer=http%3A%2F%2Fimg.pconline.com.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1631447252&t=c8dc2fab9e98a0ccb184f695d135be0d" alt="" />
-                    </li>
-                </ul>
+class LeftMain extends Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            timer: null,
+            swiperIndex: 0,
+            rotateDeg: 0,
+            thumbList: [
+                "https://img0.baidu.com/it/u=1783627040,2442271822&fm=26&fmt=auto&gp=0.jpg",
+                "https://img0.baidu.com/it/u=251614576,2693916083&fm=26&fmt=auto&gp=0.jpg",
+                "https://img0.baidu.com/it/u=103721101,4076571305&fm=26&fmt=auto&gp=0.jpg",
+                "https://img0.baidu.com/it/u=2849358178,3144634908&fm=26&fmt=auto&gp=0.jpg"
+            ]
+        }
+    }
+    componentWillMount() {
+
+    }
+
+    componentDidMount() {
+        this.autoSwiper()
+    }
+
+    // 自动转换
+    autoSwiper = () => {
+        this.setState({
+            timer: setInterval(() => {
+                if (this.state.swiperIndex <= 2) {
+                    this.setState({
+                        swiperIndex: this.state.swiperIndex + 1,
+                        rotateDeg: this.state.rotateDeg + 90
+                    })
+                } else {
+                    this.setState({
+                        swiperIndex: 0,
+                        rotateDeg: this.state.rotateDeg + 90
+                    })
+                }
+
+                console.log(this.state.swiperIndex)
+            }, 3000)
+        })
+    }
+
+    prevSwiper = () => {
+        clearInterval(this.state.timer)
+        this.setState({
+            timer: null
+        })
+        if (this.state.swiperIndex >= 1) {
+            this.setState({
+                swiperIndex: this.state.swiperIndex - 1,
+                rotateDeg: this.state.rotateDeg - 90
+            })
+        } else {
+            this.setState({
+                swiperIndex: 3,
+                rotateDeg: this.state.rotateDeg - 90
+            })
+        }
+        this.autoSwiper()
+    }
+
+    nextSwiper = () => {
+        clearInterval(this.state.timer)
+        this.setState({
+            timer: null
+        })
+        if (this.state.swiperIndex <= 2) {
+            this.setState({
+                swiperIndex: this.state.swiperIndex + 1,
+                rotateDeg: this.state.rotateDeg + 90
+            })
+        } else {
+            this.setState({
+                swiperIndex: 0,
+                rotateDeg: this.state.rotateDeg + 90
+            })
+        }
+        this.autoSwiper()
+    }
+
+    toggleSwiper = (index:number) => {
+        console.log(index)
+        clearInterval(this.state.timer)
+        this.setState({
+            timer: null
+        })
+        let newRotate:number = (index - this.state.swiperIndex) * 90
+        this.setState({
+            swiperIndex: index,
+            rotateDeg: this.state.rotateDeg + newRotate
+        })
+        this.autoSwiper()
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.state.timer)
+        this.setState({
+            timer: null
+        })
+    }
+
+    render() {
+        return (
+            <div className="left_main">
+                {/* 轮播 */}
+                <div className="swiper_box">
+                    <ul className="swiper_img" style={{ transform: `rotateY(` + this.state.rotateDeg + 'deg)' }}>
+                        {
+                            this.state.thumbList.map((item: string) => {
+                                return (
+                                    <li>
+                                        <img src={item} alt="轮播图" />
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                    <div className="arrow-box">
+                        <LeftOutlined onClick={this.prevSwiper} />
+                        <RightOutlined onClick={this.nextSwiper} />
+                    </div>
+                    <div className="squre-box">
+                        <span className={this.state.swiperIndex == 0 ? 'squre active' : 'squre'} onClick={ () => this.toggleSwiper(0)}></span>
+                        <span className={this.state.swiperIndex == 1 ? 'squre active' : 'squre'} onClick={ () => this.toggleSwiper(1)}></span>
+                        <span className={this.state.swiperIndex == 2 ? 'squre active' : 'squre'} onClick={ () => this.toggleSwiper(2)}></span>
+                        <span className={this.state.swiperIndex == 3 ? 'squre active' : 'squre'} onClick={ () => this.toggleSwiper(3)}></span>
+                    </div>
+                </div>
+                {/* 小型图文 */}
+                <div className="swiper_article">
+                    <ul>
+                        <li>
+                            <span>js防抖与节流的区别及实现</span>
+
+                            <img src="https://img0.baidu.com/it/u=103721101,4076571305&fm=26&fmt=auto&gp=0.jpg" alt="" />
+                            <i></i>
+                        </li>
+                        <li>
+                            <span>react的widthRouter</span>
+
+                            <img src="https://img0.baidu.com/it/u=103721101,4076571305&fm=26&fmt=auto&gp=0.jpg" alt="" />
+                            <i></i>
+                        </li>
+                    </ul>
+                </div>
+                {/* 技术分享 + 部分算法 */}
             </div>
-            <div className="swiper_article">
-                <ul>
-                    <li>1</li>
-                    <li>2</li>
-                </ul>
-            </div>
-        </div>
-    );
+        )
+    }
 }
+
+
+// const LeftMain = () => {
+//     let timer: any = null
+//     let swiperIndex: number = 0
+//     let init: number = 0
+//     const [sindex,setIndex] = useState(0)
+//     const [rotateDeg, setRotateDeg] = useState(0)
+//     // const [tiemr1, setTimer1] = useState({})
+//     useEffect(() => {
+//         // setTimer1(
+//         //     setInterval(() => {
+//         //         console.log("zzz")
+//         //     }, 2000)
+//         // )
+//         timer = setInterval(() => {
+//             swiperIndex += 1
+//             if (swiperIndex == 4) swiperIndex = 0
+//             console.log(swiperIndex)
+//             setIndex(swiperIndex)
+//             init += 90
+//             setRotateDeg(init)
+//         }, 3000)
+//         return () => {
+//             clearInterval(timer)
+//             // setTimer1(setInterval(() => {
+//             // }, 10000000))
+//             // clearInterval(tiemr1)
+//         }
+//     }, [])
+
+//     const prevSwiper = () => {
+//         clearInterval(timer)
+//         init -= 90
+//         setRotateDeg(init)
+
+//         timer = setInterval(() => {
+//             swiperIndex += 1
+//             if (swiperIndex == 4) swiperIndex = 0
+//             console.log(swiperIndex)
+//             init += 90
+//             setRotateDeg(init)
+//         }, 3000)
+//     }   
+//     const nextSwiper = () => {
+//         console.log(rotateDeg)
+//         clearInterval(timer)
+//         timer = null
+//         init = rotateDeg + 90
+//         setRotateDeg(init)
+
+//         // timer = setInterval(() => {
+//         //     swiperIndex += 1
+//         //     if (swiperIndex == 4) swiperIndex = 0
+//         //     console.log(swiperIndex)
+//         //     init += 90
+//         //     setRotateDeg(init)
+//         // }, 3000)
+//     }
+
+//     return (
+// <div className="left_main">
+//     <div className="swiper_box">
+//         <ul className="swiper_img" style={{ transform: `rotateY(` + rotateDeg + 'deg)' }}>
+//             <li>
+//                 <img src="https://img0.baidu.com/it/u=1783627040,2442271822&fm=26&fmt=auto&gp=0.jpg" alt="" />
+//             </li>
+//             <li>
+//                 <img src="https://img0.baidu.com/it/u=251614576,2693916083&fm=26&fmt=auto&gp=0.jpg" alt="" />
+//             </li>
+//             <li>
+//                 <img src="https://img0.baidu.com/it/u=103721101,4076571305&fm=26&fmt=auto&gp=0.jpg" alt="" />
+//             </li>
+//             <li>
+//                 <img src="https://img0.baidu.com/it/u=2849358178,3144634908&fm=26&fmt=auto&gp=0.jpg" alt="" />
+//             </li>
+//         </ul>
+//         <div className="arrow-box">
+//             <LeftOutlined onClick={() => prevSwiper()} />
+//             <RightOutlined onClick={() => nextSwiper()} />
+//         </div>
+//         <div className="squre-box">
+//             <span className={sindex == 0 ? 'squre active' : 'squre'}></span>
+//             <span className={sindex == 1 ? 'squre active' : 'squre'}></span>
+//             <span className={sindex == 2 ? 'squre active' : 'squre'}></span>
+//             <span className={sindex == 3 ? 'squre active' : 'squre'}></span>
+//         </div>
+//     </div>
+//     <div className="swiper_article">
+//         <ul>
+//             <li>
+//                 <span>js防抖与节流的区别及实现</span>
+
+//                 <img src="https://img0.baidu.com/it/u=103721101,4076571305&fm=26&fmt=auto&gp=0.jpg" alt="" />
+//                 <i></i>
+//             </li>
+//             <li>
+//                 <span>react的widthRouter</span>
+
+//                 <img src="https://img0.baidu.com/it/u=103721101,4076571305&fm=26&fmt=auto&gp=0.jpg" alt="" />
+//                 <i></i>
+//             </li>
+//         </ul>
+//     </div>
+// </div>
+//     );
+// }
 
 export default LeftMain;
--- src/components/RightMain/RightMain.tsx
+++ src/components/RightMain/RightMain.tsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import './index.css'
+import {Popover, Button} from 'antd'
+import {
+    WechatOutlined,QqOutlined,GithubOutlined
+} from '@ant-design/icons';
+
 function RightMain() {
+    const content = (
+        <div>
+            <img style={{height: '160px',width: '130px'}} src="https://img0.baidu.com/it/u=1783627040,2442271822&fm=26&fmt=auto&gp=0.jpg" alt="" />
+        </div>
+    )
+
     return (
         <div className="right_main">
-            right
+            {/* 个人介绍 */}
+            <div className="introduce_box">
+                <h2 className="title">
+                    郑～的个人简介
+                </h2>
+                <div className="item">
+                    <span>职业：</span>
+                    <span>程序员，前端开发工程师</span>
+                </div>
+                <div className="item">
+                    <span>现居：</span>
+                    <span>重庆市-沙坪坝区</span>
+                </div>
+                <div className="item">
+                    <span>Email：</span>
+                    <span>1223363269@qq.com</span>
+                </div>
+                <div className="btn_box">
+                    <Popover content={content} trigger="hover">
+                        <WechatOutlined className="btn" />
+                    </Popover>
+                    <Popover content={content} trigger="hover">
+                        <QqOutlined className="btn"  />
+                    </Popover>
+                    <Popover content={content} title="Title" trigger="hover">
+                        <GithubOutlined className="btn"  />
+                    </Popover>
+                    
+                </div>
+            </div>
+
+            {/* 排行榜 */}
+            <div>排行榜</div>
+            {/* 推荐 */}
+            <div>推荐</div>
         </div>
     )
 }
